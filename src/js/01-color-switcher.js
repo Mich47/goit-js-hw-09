b45cb7c7e1d71c13f8fb603568492287af52a4a8
Dmytro Muzyka <47075277+Mich47@users.noreply.github.com>
2022-11-01 21:58:48 +0200

    add task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+let intervalID;
 
+btnStop.disabled = true;
+btnStart.style.cursor = 'pointer';
+btnStop.style.cursor = 'pointer';
+
+btnStart.addEventListener('click', () => {
+  intervalID = setInterval(setBodyBGColor, 1000);
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+});
+
+btnStop.addEventListener('click', () => {
+  clearInterval(intervalID);
+  btnStart.disabled = false;
+  btnStop.disabled = true;
+});
+
+function setBodyBGColor() {
+  window.document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
